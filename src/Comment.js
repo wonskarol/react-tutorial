a03f61f3ff46bc5bda47680ba746170d2a0d5344
Karol Wons <karol.wons@capgemini.com>
2016-02-14 21:18:15 +0100

    we use es2015(babel) now

---
+++ src/Comment.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Comment extends React.Component {
+    rawMarkup() {
+        let rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+        return {__html: rawMarkup};
+    }
+
+    render() {
+        return <div className="comment">
+            <h2 className="commentAuthor">
+                {this.props.author}
+            </h2>
+            <span dangerouslySetInnerHTML={this.rawMarkup()}/>
+        </div>
+    }
+}
