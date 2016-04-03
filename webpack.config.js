64a50acb47dc203ec6c89ce35900af994e798c0a
Karol Wons <karol.wons@capgemini.com>
2016-04-03 10:37:07 +0200

    use webpack instead of gulp

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry:  __dirname + "/src/index.js",
+    output: {
+        path: __dirname + "/public/dist",
+        publicPath: "/dist",
+        filename: "bundle.js"
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    devServer: {
+        contentBase: "./public",
+        colors: true,
+        historyApiFallback: true,
+        inline: true,
+        hot: true,
+        proxy: {
+            '/api/*': "http://localhost:3000"
+        }
+    }
+};
+
+
