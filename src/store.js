a5b874675a40960d95da31e04349d1c226932c1b
Karol Wons <karol.wons@capgemini.com>
2016-04-17 18:21:42 +0200

    add dev redux extension

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers } from 'redux';
 import data from './reducers';
 
-const store = createStore(combineReducers({data}));
+const store = createStore(combineReducers({data}), {}, window.devToolsExtension ? window.devToolsExtension() : undefined);
 
 export default store;
